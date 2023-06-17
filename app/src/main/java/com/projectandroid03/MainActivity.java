41a7a6238043a1a6264ec9910459f890d71d703e
tuanthanh-2 <tuanthanh29263@gmail.com>
2023-06-17 21:50:41 +0700

    gg

--- app/src/main/java/com/projectandroid03/MainActivity.java
+++ app/src/main/java/com/projectandroid03/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     String b = "h";
     String c = "2";
+    String thanh = "pc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
