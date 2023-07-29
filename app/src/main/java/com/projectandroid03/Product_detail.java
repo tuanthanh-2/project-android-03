764d646f7bf9046d7b5023de39455fe2830a4629
lupin1811 <lupinnguyen1811@gmail.com>
2023-07-30 04:41:45 +0700

    +++

---
+++ app/src/main/java/com/projectandroid03/Product_detail.java
@@ -0,0 +1,13 @@
+package com.projectandroid03;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Product_detail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
